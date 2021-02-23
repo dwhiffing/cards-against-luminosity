e4da16b9544ba28cf2177556a2650e54d800991a
Daniel Whiffing <danielwhiffing@gmail.com>
2021-02-23 14:42:42 -0500

    Allows basic swapping between lists

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -13,95 +13,77 @@ const VALUES = new Array(12).fill('').map((n, i) => i + 1)
 const CARDS = new Array(Math.ceil((BOARD_SIZE * BOARD_SIZE) / 12))
   .fill(VALUES)
   .flat()
-  .map((n) => [{ value: n }])
-  .flat()
   .slice(0, BOARD_SIZE * BOARD_SIZE)
 
 const EMPTY_CARDS = new Array(BOARD_SIZE * BOARD_SIZE).fill('').map((n, i) => ({
   index: i,
-  isEmpty: true,
   list: 'board',
+  value: -1,
 }))
 
 export const Cards = () => {
   const [activeCard, setActiveCard] = useState(null)
-  const [hand, setHand] = useState(
-    shuffle(CARDS)
-      .map((n, index) => ({ ...n, index }))
-      .slice(0, 10)
-      .map((c) => ({ ...c, list: 'hand' })),
-  )
-  const [board, setBoard] = useState(EMPTY_CARDS)
-  const cards = { hand, board }
+  const [state, setState] = useState({
+    hand: shuffle(CARDS).map((n, index) => ({ value: n, index, list: 'hand' })),
+    board: EMPTY_CARDS,
+  })
 
   const { cursorState } = useMouse({
     onMouseDown: ({ clientX, clientY }) => {
-      if (!activeCard) {
-        let handCard = getCardFromPoint(clientX, clientY, 'hand', cards)
-        let boardCard = getCardFromPoint(clientX, clientY, 'board', cards)
-        let card = handCard || boardCard
-        if (card?.canMove && !card?.isEmpty) setActiveCard(card)
-        return
-      }
-
-      let otherCard = getCardFromPoint(
-        clientX,
-        clientY,
-        activeCard.list === 'hand' ? 'board' : 'hand',
-        cards,
-      )
       let element = document.elementFromPoint(clientX, clientY)
       if (element.classList.contains('click')) {
         element = element.parentElement
       }
 
-      const { list, index } = element.dataset
+      const { list: bList, index: bIndex } = element.dataset
+      const cardB =
+        bList && typeof bIndex === 'string' ? state[bList][+bIndex] : null
 
-      if (activeCard.list === 'board') {
-        // move card from board to hand
-        if (list === 'hand') {
-          setActiveCard(null)
-          setHand(addItem({ ...activeCard, list: 'hand' }, hand.length))
-          setBoard(removeItem(activeCard.index))
-        }
-        // move card from board to board
-        if (typeof index === 'string' && +index !== +activeCard.index) {
-          setActiveCard(null)
-          setBoard((cards) => {
-            cards = cards.map((c) =>
-              c.index === activeCard.index
-                ? { index: c.index, isEmpty: true }
-                : c.index === +index
-                ? { ...activeCard, index: c.index }
+      if (!cardB) return setActiveCard(null)
+      if (!activeCard) return setActiveCard(cardB)
+
+      const { value: bValue } = cardB
+      const { list: aList, index: aIndex, value: aValue } = activeCard
+
+      if (aList === bList) {
+        // moving within the same list
+        setState((state) => {
+          const newState = {
+            ...state,
+            [bList]: state[bList].map((c) =>
+              c.index === +bIndex
+                ? { value: aValue, index: +bIndex, list: bList }
+                : c.index === +aIndex
+                ? { value: bValue, index: +aIndex, list: bList }
                 : c,
-            )
-
-            return cards
-          })
-        }
-      } else if (otherCard?.index) {
-        // move card from hand to board
-        setActiveCard(null)
-        setBoard(addItem({ ...activeCard, list: 'board' }, otherCard?.index))
-        setHand((c) =>
-          c
-            .filter((c) => c.index !== activeCard.index)
-            .map((c, i) => ({ ...c, index: i })),
-        )
+            ),
+          }
+          return newState
+        })
       }
+      // moving to another list
+      else
+        setState((state) => {
+          const newState = {
+            ...state,
+            [aList]: state[aList].map((c) =>
+              c.index === +aIndex
+                ? { value: bValue, index: +aIndex, list: aList }
+                : c,
+            ),
+            [bList]: state[bList].map((c) =>
+              c.index === +bIndex
+                ? { value: aValue, index: +bIndex, list: bList }
+                : c,
+            ),
+          }
+          return newState
+        })
+
+      setActiveCard(null)
     },
     onMouseUp: ({ clientX, clientY }) => {},
   })
-
-  const removeItem = (index) => (cards) =>
-    cards.map((card) =>
-      card?.index !== index ? card : { index: card.index, isEmpty: true },
-    )
-  const addItem = (card, index) => (cards) => [
-    ...cards.slice(0, index),
-    { ...card, index },
-    ...cards.slice(index + 1),
-  ]
 
   useWindowEvent('resize', debounce(useForceUpdate(), 500))
 
@@ -109,13 +91,13 @@ export const Cards = () => {
     <div className="cards-container">
       <List
         name="board"
-        cards={cards.board}
+        cards={state.board}
         activeCard={activeCard}
         cursorState={cursorState}
       />
       <List
         name="hand"
-        cards={cards.hand}
+        cards={state.hand}
         activeCard={activeCard}
         cursorState={cursorState}
       />
@@ -128,17 +110,24 @@ const List = ({ name, cards, activeCard, cursorState }) => {
     <div style={{ flex: 1 }} data-list={name}>
       {cards.map((card, cardIndex) => {
         if (!card) return null
-        const isActive = activeCard ? activeCard.index === card.index : false
-        const { x, y } = getCardPosition(card)
+        const isActive = activeCard
+          ? activeCard.index === card.index && activeCard.list === card.list
+          : false
+        const xBuffer = window.innerWidth / 2 - BOARD_SIZE * 25
+        const yBuffer = window.innerHeight / 2 - BOARD_SIZE * 50
+        let x = (card.index % BOARD_SIZE) * CARD_HEIGHT + xBuffer
+        let y = Math.floor(card.index / BOARD_SIZE) * CARD_HEIGHT + yBuffer
+        // TODO: Gross
+        if (card.list === 'hand') y += 400
 
         return (
           <Card
             key={`card-${cardIndex}`}
             list={name}
             card={card}
-            isActive={isActive && !card.isEmpty}
-            x={isActive && !card.isEmpty ? cursorState.mouseX : x}
-            y={isActive && !card.isEmpty ? cursorState.mouseY : y}
+            isActive={isActive && card.value !== -1}
+            x={isActive && card.value !== -1 ? cursorState.mouseX : x}
+            y={isActive && card.value !== -1 ? cursorState.mouseY : y}
           />
         )
       })}
@@ -156,7 +145,7 @@ const Card = React.memo(
     const classes = [
       `card can-move rank${card.value}`,
       isActive && 'disable-touch',
-      card.isEmpty && 'empty',
+      card.value === -1 && 'empty',
     ]
 
     return (
@@ -171,7 +160,6 @@ const Card = React.memo(
               zIndex: 10,
             }}
           >
-            {/* <div className="face" /> */}
             <div className="back" />
             <div className="click" />
           </div>
@@ -180,30 +168,6 @@ const Card = React.memo(
     )
   },
 )
-
-const getCardFromPoint = (x, y, tag, cards) => {
-  const elementUnder = document.elementFromPoint(x, y)
-  const dataIndex = elementUnder?.parentElement?.dataset?.index
-  const card = cards[tag][+dataIndex]
-
-  if (!dataIndex || !card) return null
-
-  return {
-    ...card,
-    ...getCardPosition(card),
-    canMove: true,
-  }
-}
-
-const getCardPosition = (card) => {
-  const xBuffer = window.innerWidth / 2 - BOARD_SIZE * 25
-  const yBuffer = window.innerHeight / 2 - BOARD_SIZE * 50
-  let x = (card.index % BOARD_SIZE) * CARD_HEIGHT + xBuffer
-  let y = Math.floor(card.index / BOARD_SIZE) * CARD_HEIGHT + yBuffer
-  // TODO: Gross
-  if (card.list === 'hand') y += 400
-  return { x, y }
-}
 
 const useMouse = ({ onMouseDown: _onMouseDown, onMouseUp: _onMouseUp }) => {
   const [cursorState, setCursorState] = useState(initialState)
