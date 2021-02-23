15953ffe0a2bed4575aa787d5e09fdca7604691c
Daniel Whiffing <danielwhiffing@gmail.com>
2021-02-23 08:05:15 -0500

    wip

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -9,27 +9,130 @@ import chunk from 'lodash/chunk'
 import groupBy from 'lodash/groupBy'
 
 const SUITS = ['spades', 'clubs', 'hearts', 'diamonds']
-
 const config = { stiffness: 200, damping: 20 }
+const initialState = { mouseY: 0, mouseX: 0 }
+const VALUES = '987654321abcdefghijklmnopqrstuvwxyz'
+const CARDS = VALUES.split('')
+  .map((n) => [
+    { value: Number(n), suit: 0 },
+    { value: Number(n), suit: 1 },
+    { value: Number(n), suit: 2 },
+    { value: Number(n), suit: 3 },
+    { value: Number(n), suit: 4 },
+  ])
+  .flat()
+  .slice(0, 144)
+const EMPTY_CARDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => ({
+  cardPileIndex: -1,
+  pileIndex: n,
+  isEmpty: true,
+}))
+
+export const Cards = () => {
+  const [activeCard, setActiveCard] = useState(null)
+  const [cursorState, setCursorState] = useState(initialState)
+  const startRef = useRef({ x: 0, y: 0 })
+  const deltaRef = useRef({ x: 0, y: 0 })
+  const [pressed, setPressed] = useState(false)
+  const [cards, setCards] = useState(shuffleDeck())
+
+  const onMouseDown = ({ clientX, clientY }) => {
+    let card = getCardFromPoint(clientX, clientY, cards)
+
+    if (!card) {
+      return setActiveCard(null)
+    }
+
+    if (activeCard) {
+      setCards(moveCard(cards, activeCard, card))
+      setActiveCard(null)
+    } else if (!card.isActive && card.canMove) {
+      setActiveCard(card)
+    }
+    const mouseY = card.y
+    const mouseX = card.x
+    startRef.current = { x: clientX, y: clientY }
+    deltaRef.current = { x: clientX - card.x, y: clientY - card.y }
+    setPressed(true)
+
+    setCursorState({ mouseX, mouseY })
+  }
+
+  const onMouseMove = ({ clientY, clientX }) => {
+    const mouseY = clientY - deltaRef.current.y
+    const mouseX = clientX - deltaRef.current.x
+    setCursorState({ mouseY, mouseX })
+  }
+
+  const onMouseUp = ({ clientX, clientY }) => {
+    const diffX = Math.abs(startRef.current.x - clientX)
+    const diffY = Math.abs(startRef.current.y - clientY)
+    const passedThreshold = diffX > 15 || diffY > 15
+
+    deltaRef.current = { x: 0, y: 0 }
+    setPressed(false)
+
+    if (activeCard) {
+      let clickedCard = getCardFromPoint(clientX, clientY, cards)
+      if (clickedCard) {
+        setCards(moveCard(cards, activeCard, clickedCard))
+      }
+      if (passedThreshold) {
+        setActiveCard(null)
+      }
+    }
+  }
+
+  useWindowEvent('resize', debounce(useForceUpdate(), 500))
+  useWindowEvent('pointerup', onMouseUp)
+  useWindowEvent('pointerdown', onMouseDown)
+  useWindowEvent('pointermove', onMouseMove)
+  return (
+    <div
+      style={{
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ position: 'relative', width: '25rem', height: '25rem' }}>
+        {[0, 1, 2, 3, 4, 5].map((n) => (
+          <Card key={`pile-${n}`} card={EMPTY_CARDS[n]} />
+        ))}
+
+        {cards.map((card, cardIndex) => {
+          const isActive = getCardIsActive(activeCard, card)
+
+          return (
+            <Card
+              key={`card-${cardIndex}`}
+              card={card}
+              isActive={isActive}
+              activeCard={activeCard}
+              mouseX={isActive && pressed ? cursorState.mouseX : -1}
+              mouseY={isActive && pressed ? cursorState.mouseY : -1}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
 
 const Card = React.memo(
   ({
     card,
     activeCard,
-    pileSize,
     isActive,
     isFinished,
-    canMove,
     onRest = () => {},
     mouseX = -1,
     mouseY = -1,
   }) => {
     useWindowEvent('resize', debounce(useForceUpdate(), 500))
-    const { height, xBuffer, width } = getCardSpacing(pileSize)
-    const { x: xPos, y: yPos } = getCardPosition(
-      { ...card, isFinished },
-      pileSize,
-    )
+    const { height, xBuffer, width } = getCardSpacing()
+    const { x: xPos, y: yPos } = getCardPosition({ ...card, isFinished })
 
     const yOffset =
       mouseX > -1
@@ -50,7 +153,7 @@ const Card = React.memo(
       'card',
       `rank${card.value}`,
       isFinished && 'finished',
-      canMove && 'can-move',
+      'can-move',
       isActive && 'disable-touch',
       card.isEmpty && 'empty',
       SUITS[card.suit],
@@ -86,143 +189,6 @@ const Card = React.memo(
     )
   },
 )
-
-const initialState = { mouseY: 0, mouseX: 0 }
-const EMPTY_CARDS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => ({
-  cardPileIndex: -1,
-  pileIndex: n,
-  isEmpty: true,
-}))
-export const Cards = () => {
-  const [activeCard, setActiveCard] = useState(null)
-  const [cursorState, setCursorState] = useState(initialState)
-  const startRef = useRef({ x: 0, y: 0 })
-  const deltaRef = useRef({ x: 0, y: 0 })
-  const [finishedPiles, setFinishedPiles] = useState([])
-  const [pressed, setPressed] = useState(false)
-  const [cards, setCards] = useState(shuffleDeck())
-  const [hasWon, setHasWon] = useState(false)
-
-  useEffect(() => {
-    const newFinishedPiles = checkForFinishedPiles(cards)
-    if (!hasWon && newFinishedPiles.length !== finishedPiles.length) {
-      if (newFinishedPiles.length >= 4) {
-        setHasWon(true)
-      }
-      setTimeout(() => setFinishedPiles(newFinishedPiles), 500)
-    }
-  }, [cards, finishedPiles, hasWon])
-
-  const onMouseDown = ({ clientX, clientY }) => {
-    let card = getCardFromPoint(clientX, clientY, cards)
-
-    if (!card) {
-      return setActiveCard(null)
-    }
-
-    if (activeCard) {
-      const bottomCard = getBottomCard(card, cards)
-      setCards(moveCard(cards, activeCard, bottomCard))
-
-      setActiveCard(null)
-    } else if (!card.isActive && card.canMove) {
-      setActiveCard(card)
-    }
-    const mouseY = card.y
-    const mouseX = card.x
-    startRef.current = { x: clientX, y: clientY }
-    deltaRef.current = { x: clientX - card.x, y: clientY - card.y }
-    setPressed(true)
-
-    setCursorState({ mouseX, mouseY })
-  }
-
-  const onMouseMove = ({ clientY, clientX }) => {
-    const mouseY = clientY - deltaRef.current.y
-    const mouseX = clientX - deltaRef.current.x
-    setCursorState({ mouseY, mouseX })
-  }
-
-  const onMouseUp = ({ clientX, clientY }) => {
-    const diffX = Math.abs(startRef.current.x - clientX)
-    const diffY = Math.abs(startRef.current.y - clientY)
-    const passedThreshold = diffX > 15 || diffY > 15
-
-    deltaRef.current = { x: 0, y: 0 }
-    setPressed(false)
-
-    if (activeCard) {
-      let clickedCard = getCardFromPoint(clientX, clientY, cards)
-      clickedCard = getBottomCard(clickedCard, cards)
-      if (clickedCard) {
-        setCards(moveCard(cards, activeCard, clickedCard))
-      }
-      if (passedThreshold) {
-        setActiveCard(null)
-      }
-    }
-  }
-
-  useWindowEvent('resize', debounce(useForceUpdate(), 500))
-  useWindowEvent('pointerup', onMouseUp)
-  useWindowEvent('pointerdown', onMouseDown)
-  useWindowEvent('pointermove', onMouseMove)
-  return (
-    <div
-      style={{
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <div style={{ position: 'relative', width: '25rem', height: '25rem' }}>
-        {[0, 1, 2, 3, 4, 5].map((n) => (
-          <Card key={`pile-${n}`} card={EMPTY_CARDS[n]} />
-        ))}
-
-        {cards.map((card, cardIndex) => {
-          const isActive = getCardIsActive(activeCard, card)
-
-          return (
-            <Card
-              key={`card-${cardIndex}`}
-              card={card}
-              activeCard={activeCard}
-              isActive={isActive}
-              pileSize={getCardPile(card, cards).length}
-              canMove={getCanCardMove(card, cards)}
-              isFinished={finishedPiles.includes(card.pileIndex)}
-              mouseX={
-                getCardIsActive(activeCard, card) && pressed
-                  ? cursorState.mouseX
-                  : -1
-              }
-              mouseY={
-                getCardIsActive(activeCard, card) && pressed
-                  ? cursorState.mouseY
-                  : -1
-              }
-            />
-          )
-        })}
-      </div>
-    </div>
-  )
-}
-
-const VALUES = '987654321abcdefghijklmnopqrstuvwxyz'
-
-const CARDS = VALUES.split('')
-  .map((n) => [
-    { value: Number(n), suit: 0 },
-    { value: Number(n), suit: 1 },
-    { value: Number(n), suit: 2 },
-    { value: Number(n), suit: 3 },
-    { value: Number(n), suit: 4 },
-  ])
-  .flat()
-  .slice(0, 144)
 
 const shuffleDeck = () =>
   chunk(shuffle(CARDS), 12)
@@ -313,7 +279,7 @@ const checkForFinishedPiles = (cards) => {
     .map((pile) => pile.index)
 }
 
-export function getCardIsActive(activeCard, card) {
+function getCardIsActive(activeCard, card) {
   let isActive = false
 
   if (activeCard) {
@@ -339,20 +305,6 @@ const decorateCard = (card, cards) =>
         isActive: getCardIsActive(card, cards),
       }
     : null
-
-const getBottomCard = (card, cards) => {
-  if (!card) {
-    return null
-  }
-
-  if (card.isEmpty) {
-    return card
-  }
-
-  const pile = getCardPile(card, cards)
-  card = pile[pile.length - 1]
-  return decorateCard(card, cards)
-}
 
 const getCanCardMove = (card, cards) => {
   const pile = getCardPile(card, cards)
@@ -386,7 +338,7 @@ const getCardFromPoint = (x, y, cards) => {
   return decorateCard(card, cards)
 }
 
-const getCardSpacing = (pileSize = 0) => {
+const getCardSpacing = () => {
   // const outerWidth = clamp(document.documentElement.clientWidth, 740)
   // const outerHeight = clamp(document.documentElement.clientHeight, 740)
   const width = 50
@@ -396,8 +348,8 @@ const getCardSpacing = (pileSize = 0) => {
   return { width, height, yBuffer, xBuffer }
 }
 
-const getCardPosition = (card, pileSize) => {
-  const { width, height, yBuffer, xBuffer } = getCardSpacing(pileSize)
+const getCardPosition = (card) => {
+  const { width, height, yBuffer, xBuffer } = getCardSpacing()
 
   const x = card.pileIndex * width + xBuffer
 
